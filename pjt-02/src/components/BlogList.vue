1cde7570f5744bd94414bc55b40cbab7f4e9bc23
yoony <{by.cho.dev@gmail.com}>
2022-11-15 22:47:37 +0900

    route

---
+++ pjt-02/src/components/BlogList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="data in blogDesc" :key="data">
+    <h5>{{ data.title }}</h5>
+    <p>{{data.content}}</p>
+    <p>{{data.date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogList',
+  props : {
+    blogDesc : Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
