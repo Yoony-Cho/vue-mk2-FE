bdb62e8748201de43fba14ee0fe16f3f93de9844
Yoony-Cho <by.cho.dev@gmail.com>
2022-11-15 23:06:18 +0900

    test

--- pjt-02/src/App.vue
+++ pjt-02/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Dev Sauros TestTestTestTestTestTestTestTestTestTestTestTestTest</a>
+        <a class="navbar-brand" href="#">Dev Sauros</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
