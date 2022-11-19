99b07ad4c465adca5ce79ac083278d823925df2b
Yoony-Cho <by.cho.dev@gmail.com>
2022-11-19 23:02:42 +0900

    parameter

---
+++ pjt-02/src/components/BlogDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  $route.params.id
+  <div>
+    <h4>상세페이지</h4>
+    <h5>{{ blogDesc[$route.params.id].title }}</h5>
+    <p>{{ blogDesc[$route.params.id].content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BlogDetail',
+    props : {
+        blogDesc : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pjt-02/src/router.js
+++ pjt-02/src/router.js
@@ -1,10 +1,15 @@
 import { createWebHistory, createRouter } from "vue-router";
 import BlogList from './components/BlogList.vue';
+import BlogDetail from './components/BlogDetail.vue';
 
 const routes = [
   {
     path: "/list",
     component: BlogList,
+  },
+  {
+    path: "/detail/:id",
+    component: BlogDetail,
   }
 ];
 
